f73558d7fc37247c5063f8d08ca1f9c39d22def8
hello2all <hellot2@hotmail.com>
2017-06-16 23:07:09 +0800

    setup initial pid control

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,8 @@
 #include "PID.h"
 
+//DEBUG
+#include <iostream>
+
 using namespace std;
 
 /*
@@ -10,12 +13,61 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double kp, double ki, double kd) {
+  Kp = kp;
+  Ki = ki;
+  Kd = kd;
+
+
+  current_time = time(0);
+  last_time = current_time;
+  Clear();
 }
 
 void PID::UpdateError(double cte) {
+
+  totoal_error += cte;
+
+  current_time = time(0);
+  double delta_t = current_time - last_time;
+  double delta_err = cte - last_error;
+
+  p_error = cte;
+  i_error += cte * delta_t;
+
+  if(i_error >= windup_guard)
+    i_error = -windup_guard;
+  
+  else if(i_error > windup_guard)
+    i_error = windup_guard;
+
+  d_error = 0;
+  if(delta_t > 0)
+    d_error = delta_err / delta_t;
+  
+  last_time = current_time;
+  last_error = cte;
+}
+
+void PID::Clear(){
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+
+  last_error = 0;
+  init_error = 0;
+  windup_guard = 20;
+  
+  output = 0;
 }
 
 double PID::TotalError() {
+
+  double steer = -Kp * p_error - Ki * i_error - Kd * d_error;
+
+  if(steer > 1.0) steer = 1.0;
+  if(steer < -1.0) steer = -1.0;
+
+  return steer;
 }
 
--- src/PID.h
+++ src/PID.h
@@ -1,6 +1,8 @@
 #ifndef PID_H
 #define PID_H
 
+#include <ctime>
+
 class PID {
 public:
   /*
@@ -11,11 +13,29 @@ public:
   double d_error;
 
   /*
+  * Time
+  */
+  long double current_time;
+  long double last_time;
+
+  /*
   * Coefficients
   */ 
   double Kp;
   double Ki;
   double Kd;
+
+  /*
+  * Errors
+  */
+  
+  double last_error;
+  double init_error;
+  double totoal_error;
+
+  double windup_guard;
+  double output;
+
 
   /*
   * Constructor
@@ -41,6 +61,11 @@ public:
   * Calculate the total PID error.
   */
   double TotalError();
+
+  /*
+  * Clear internal parameters
+  */
+  void Clear();
 };
 
 #endif /* PID_H */
